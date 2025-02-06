72d653f1d3889caa0c7cee2c184ebaa376b0b532
Kaustubh Kumar <141555419+grittyuser007@users.noreply.github.com>
2025-02-07 00:50:19 +0530

    Time: 910 ms (6.58%) | Memory: 99.3 MB (15.67%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -4,8 +4,8 @@ public:
      int count=0;
      
      int lens=nums.size();
-    std::map<int, int> order;
-     for(int i=0;i<lens;i++)
+    map<int, int> order;
+     for(int i=0;i<lens-1;i++)
     {
         for(int j=i+1;j<lens;j++)
         {
