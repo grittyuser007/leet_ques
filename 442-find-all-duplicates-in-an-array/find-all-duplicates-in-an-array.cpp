c7bee7e836d0116017a45c5c0848ca42e6b3324b
Kaustubh Kumar <141555419+grittyuser007@users.noreply.github.com>
2025-02-08 02:38:20 +0530

    Time: 10 ms (50.86%) | Memory: 50.1 MB (11.06%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-       unordered_map<int,int> mep;
+       vector<int> mep(nums.size()+1,0);
      
-       int x=0;
        vector<int> v;
        if(nums.size()==1){return v;}
        for(int i=0;i<nums.size();i++)
        {
-        if(mep.count(nums[i])>0)
-        { v.push_back(nums[i]);
-        x++;
-        }
+       if(mep[nums[i]]>0)
+       {
+        v.push_back(nums[i]);
+      
+       }
 
         mep[nums[i]]++;
         
 
        }
+      // for(auto ar:mep){ cout<<ar<<endl;}
+       
        
        return v;
        
