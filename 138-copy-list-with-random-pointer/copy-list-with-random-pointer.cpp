d9570c0019aaf7c1c6007863625bebec0304c477
Kaustubh Kumar <141555419+grittyuser007@users.noreply.github.com>
2025-05-22 01:23:15 +0530

    Time: 11 ms (15.68%) | Memory: 15.3 MB (10.13%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+private:
+void ins(Node* &head,Node * &tail,int d)
+{ Node * n=new Node(d);
+    if(tail==NULL)
+    {
+       tail=n;
+       head=n;
+    }
+    else
+    {
+        tail->next=n;
+        tail=n;
+    }
+}
+
+public:
+    Node* copyRandomList(Node* head) {
+  Node *orig=head;
+  Node *cloneHead=NULL;
+  Node *cloneTail=NULL;
+  while(orig!=NULL)
+  {
+    ins(cloneHead,cloneTail,orig->val);
+    orig=orig->next;
+  }
+  orig=head;
+  Node *temp=cloneHead;
+unordered_map<Node*,Node*>mapp;
+ while(orig!=NULL && temp!=NULL)
+ {
+    mapp[orig]=temp;
+    orig=orig->next;
+    temp=temp->next;
+ }
+ orig=head;
+ temp=cloneHead;
+ while(orig!=NULL )
+ { 
+   temp->random= mapp[orig->random];
+   temp=temp->next;
+   orig=orig->next;
+ }
+
+ return cloneHead;
+    }
+};
