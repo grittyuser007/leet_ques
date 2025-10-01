cf22b9feb3db0a24fd8fd472fc8f50888c59f1d2
Kaustubh Kumar <141555419+grittyuser007@users.noreply.github.com>
2025-10-02 01:36:38 +0530

    Time: 2 ms (27.22%) | Memory: 10.3 MB (78.27%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.c
+++ 13-roman-to-integer/roman-to-integer.c
@@ -1,77 +1,59 @@
-#include <string.h>
-
 int romanToInt(char* s) {
-  int i = 0;
-  int intValue = 0;
-
-  while(i < strlen(s))
-  {
-    switch(s[i])
-    {
-      case 'I':
-        if(s[i+1] == 'V')
-        {
-          intValue += 4;
-          i++;
-        }
-        else if(s[i+1] == 'X')
-        {
-          intValue += 9;
-          i++;
-        }
-        else
-        {
-          intValue += 1;
-        }
-      break;
-      case 'V':
-        intValue += 5;
-      break;
-      case 'X':
-        if(s[i+1] == 'L')
-        {
-          intValue += 40;
-          i++;
-        }
-        else if(s[i+1] == 'C')
-        {
-          intValue += 90;
-          i++;
-        }
-        else
-        {
-          intValue += 10;
-        }
-      break;
-      case 'L':
-        intValue += 50;
-      break;
-      case 'C':
-        if(s[i+1] == 'D')
-        {
-          intValue += 400;
-          i++;
-        }
-        else if(s[i+1] == 'M')
-        {
-          intValue += 900;
-          i++;
-        }
-        else
-        {
-          intValue += 100;
-        }
-      break;
-      case 'D':
-        intValue += 500;
-      break;
-      case 'M':
-        intValue += 1000;
-      break;
-      default:
-      break;
-    }
+ int M=1000;
+  int C=100;
+  int I=1;
+  int D=500;
+  int V=5;
+   int L=50;
+   int X=10;
+  int count=0;
+  for(int i=0;i<strlen(s);i++)
+  {  switch(s[i])
+  { case 'I':
+  if(s[i+1]=='V'){
+    count=count+4;
     i++;
+    break;
   }
-  return intValue;
+  
+  else if(s[i+1]=='X'){
+    count=count+9;
+    i++;
+    break;
+  } 
+  else
+  { count+=I;
+  break;}
+  case 'V':
+  
+  {
+    count+=V;
+    break;}
+
+  case 'X':
+  if(s[i+1]=='L'){ count+=40;i++;break;}
+  else if(s[i+1]=='C'){ count+=90;i++;break;}
+  else{ count+=X; break;}
+
+  case 'L':
+  { count+=50;break;}
+
+  case 'C':
+  if(s[i+1]=='D'){count+=400;i++;break;}
+  else if(s[i+1]=='M'){count+=900;i++;break;}
+  else{ count+=C;break;}
+   
+   case 'D':
+   
+   {count+=D;break;}
+
+   case 'M':
+   count+=M;
+   break;
+    
+
+  }
+    
+  } 
+  return count; 
 }
