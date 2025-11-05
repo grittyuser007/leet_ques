c7b990ad762475141f247be3e8608c0770791563
Kaustubh Kumar <141555419+grittyuser007@users.noreply.github.com>
2025-11-06 02:52:14 +0530

    Time: 295 ms (5.76%) | Memory: 73 MB (72.94%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        stack<int> st;
+        for(auto s:nums)
+        {
+            if(st.empty())
+            {
+                st.push(s);
+                continue;
+
+            }
+            else if(st.top()==s)
+            { 
+                return true;
+            }
+            st.push(s);
+            cout<<s;
+        }
+        return false;
+    }
+};
